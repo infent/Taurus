b6951a0f9a8d8019abd7fcd90bf8f6dac0254e64
zs <1612756182@qq.com>
2020-05-27 22:05:51 +0800

    编写routes

--- vuelearn200527/src/router/index.js
+++ vuelearn200527/src/router/index.js
@@ -1,15 +1,32 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from "vue";
+import Router from "vue-router";
+import HelloWorld from "@/components/HelloWorld";
+import VueRouter from "vue-router";
 
-Vue.use(Router)
+Vue.use(Router);
 
-export default new Router({
-  routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
-  ]
-})
+// export default new Router({
+//   routes: [
+//     {
+//       path: "/",
+//       name: "HelloWorld",
+//       component: HelloWorld
+//     }
+//   ]
+// });
+
+export const constantRoutes = [
+  {
+    path:'/index',
+    component:()=>import('@/views/index.vue'),
+    hidden:false,
+  }
+]; 
+
+const createRouter = () =>
+  new VueRouter({
+    mode: "history",
+    scrollBehavior: () => ({ y: 0 }),
+    routes: constantRoutes
+  });
+const router = createRouter();
